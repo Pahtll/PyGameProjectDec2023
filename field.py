2ef12fce12527c1a892905566c63e3dd312a4ce9
4444urka <149653447+4444urka@users.noreply.github.com>
2023-11-30 00:51:10 +1000

    Обновление генерации поля

--- field.py
+++ field.py
@@ -4,6 +4,7 @@ import pygame as pg
 
 
 class Box:
+    """Коробки - это объекты, из которых строится карта """
 
     def __init__(self, coordinates):
         """Инициализация коробки"""
@@ -14,28 +15,39 @@ class Box:
         pg.draw.rect(screen, (255, 255, 255), pg.Rect(*self.coordinates, 50, 50), 2)
 
 
-def generate(countOfBoxes, screen):
-    """Генерация поля"""
-    x = random.randint(0, 800)
-    y = random.randint(0, 600)
-    box = Box((x, y))
-    box.add(screen)
+def generate(screen):
+    """Генерация случайного поля, которое состоит из случайного количества структур.
+    Каждая структура состоит из случайного количества коробок, стоящих рядом"""
+    countOfStructures = random.randint(10, 15)
+    listOfAllBoxes = set()
 
-    # Цикл генерирует последовательность присоединённых друг к другу коробок в случайном месте карты
-    for _ in range(countOfBoxes - 1):
-        position = random.randint(1, 4)
-
-        if position == 1 and x + 50 < 800:
-            x += 50
-
-        elif position == 2 and y + 50 < 600:
-            y += 50
-
-        elif position == 3 and x - 50 > 0:
-            x -= 50
-
-        elif position == 4 and y - 50 > 0:
-            y -= 50
-
+    # Первый цикл отвечает за количество структур
+    for _ in range(countOfStructures):
+        x = random.randrange(0, 800, 50)
+        y = random.randrange(0, 600, 50)
         box = Box((x, y))
+        listOfAllBoxes.add((x, y))
         box.add(screen)
+        countOfBoxes = random.randint(5, 10)
+
+        # Второй за количество коробок в структуре
+        for _ in range(countOfBoxes - 1):
+            position = random.randint(1, 4)
+
+            if position == 1 and x + 50 < 800:
+                x += 50
+
+            elif position == 2 and y + 50 < 600:
+                y += 50
+
+            elif position == 3 and x - 50 > 0:
+                x -= 50
+
+            elif position == 4 and y - 50 > 0:
+                y -= 50
+
+            # Список, который хранит в себе все данные о коробках. Он понадобится в будущем
+            listOfAllBoxes.add((x, y))
+            box = Box((x, y))
+            box.add(screen)
+    print(len(listOfAllBoxes))
