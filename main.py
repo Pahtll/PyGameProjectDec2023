a9d921c7c267e231ce2194edf7198bb03c8c673b
Vadim Denisovich <vadimDenis0vich@yandex.com>
2023-11-29 16:24:25 +1000

    Начало положено

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import pygame as pg
-
+import screeninfo
 pg.init()
+
+monitor = screeninfo.get_monitors()
+width = monitor[0].width
+height = monitor[0].height
+print(width, height)
+
+screen = pg.display.set_mode((width, height))
