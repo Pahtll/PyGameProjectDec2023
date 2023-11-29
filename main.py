d2388e399767545ad5d0fe64637c6e386cc35baf
Vadim Denisovich <vadimDenis0vich@yandex.com>
2023-11-29 16:47:30 +1000

    Обновление 1.2

--- main.py
+++ main.py
@@ -8,3 +8,16 @@ height = monitor[0].height
 print(width, height)
 
 screen = pg.display.set_mode((width, height))
+
+running = True
+while running:
+
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+    screen.fill((255, 255, 255))
+
+    pg.display.flip()
+
+pg.quit()
