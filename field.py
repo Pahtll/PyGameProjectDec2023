eaf34bfceb0bd696d7b61831b59fcc8c5f6bc775
4444urka <psinka2006@gmail.com>
2023-11-29 20:42:46 +1000

    Начал постепенную разработку логики поля, которое состоит из коробок

--- field.py
+++ field.py
@@ -1,4 +1,19 @@
 import random
+import pygame as pg
+class Box:
 
-class Field:
-    pass
+    def __init__(self, coordinates):
+        self.coordinates = coordinates
+
+    def add(self, screen):
+        pg.draw.rect(screen, (255, 255, 255), pg.Rect(*self.coordinates, 50, 50), 2)
+
+def generate(CountOfBoxes, screen):
+
+    for _ in range(CountOfBoxes):
+        box = Box((random.randint(0, 800), random.randint(0, 600)))
+        box.add(screen)
+
+
+
+
