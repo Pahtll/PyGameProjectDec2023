6d17ee2d790bb532597679e9b11853eba7663331
4444urka <psinka2006@gmail.com>
2023-11-30 13:18:47 +1000

    Обновка
    Добавлен задний фон (временный). Теперь настройки генерации поля нужно указывать в main а не в field

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pygame as pg
 import tank
 import field
-import random
 
 pg.init()
 
@@ -10,8 +9,12 @@ tank1 = tank.tank((0, 0))#Тестовый танк заглушка
 WIDTH, HEIGHT = 800, 600
 screen = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption('Чечня 1994')
-screen.fill((0, 0, 0))
-field.generate(random.randint(10, 15), random.randint(5, 10), screen)
+
+#Задний фон
+background = pg.image.load("images/background.png")
+screen.blit(background, (0, 0))
+
+field.generate((10, 30), (1, 10), screen)
 running = True
 while running:
     for event in pg.event.get():
