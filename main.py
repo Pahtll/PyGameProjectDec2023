362b0368756dbd197b3429a8414ed5b8b781d73e
4444urka <psinka2006@gmail.com>
2023-11-30 16:23:42 +1000

    Добавлены новые задние фоны
    Они выбираются рандомно

--- main.py
+++ main.py
@@ -11,7 +11,16 @@ screen = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption('bro< tanchikiiii')
 
 # Задний фон и иконка
-background = pg.image.load("images/background.png")
+randomNumber = random.randint(0, 10)
+if randomNumber == 10:
+    background = pg.image.load("images/4urka.png")
+
+elif 5 < randomNumber < 10:
+    background = pg.image.load("images/background2.png")
+
+elif 0 <= randomNumber <= 5:
+    background = pg.image.load("images/background.png")
+
 screen.blit(background, (0, 0))
 icon = pg.image.load('images/icon.png')
 pg.display.set_icon(icon)
