dc5f8e3924f1b28f9d4ed8ddaeaeabe69a2a0c15
Vadim Denisovich <vadimDenis0vich@yandex.com>
2023-12-03 18:08:50 +1000

    Исправлена неспособность танка стрелять вполтную к коробке.
    Также добавлена сложность и некоторые небольшие моменты.

---
+++ controls.py
@@ -0,0 +1,22 @@
+import tank, field
+
+def set_difficulty(range):
+    match range:
+        case 1:
+            tank.Tank.speed = 1
+            tank.Tank.hp = 200
+            tank.Bullet.damage = 50
+            tank.Bullet.speed = 5
+            field.Box.hp = 150
+        case 2:
+            tank.Tank.speed = 1
+            tank.Tank.hp = 300
+            tank.Bullet.damage = 50
+            tank.Bullet.speed = 5
+            field.Box.hp = 200
+        case 3:
+            tank.Tank.speed = 3
+            tank.Tank.hp = 420
+            tank.Bullet.damage = 40
+            tank.Bullet.speed = 8
+            field.Box.hp = 200
