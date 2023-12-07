4567e3f9a870f788d10e96dc57132e047c316392
Pahtll <astahov.egor008@gmail.com>
2023-12-07 16:26:26 +1000

    Revert "Попытка перереализации выхода в меню."
    This reverts commit bd9e965510b1c180662dc57a6b7afd2f99d81a53.

--- main.py
+++ main.py
@@ -17,103 +17,103 @@ pg.display.set_caption('bro, tanki')
 # Иконка для приложения
 icon = pg.image.load('images/icon.png')
 pg.display.set_icon(icon)
-while True:
-    # Главное меню
-    main_menu = menu.MainMenu(screen)
 
-    # Задний фон для игры
-    background = background.create_background()
+#Главное меню
+main_menu = menu.MainMenu(screen)
 
-    # Тестовое создание танка
-    tank_topleft = tank.TankTopLeft(screen, 0, 0)
-    tank_bottomright = tank.TankBottomRight(screen, 764, 558)
+# Задний фон для игры
+background = background.create_background()
 
-    # Установка сложности игры // Оставляйте 1 пока что
-    controls.set_difficulty(1)
 
-    # Пули от танка
-    bullets_topleft = Group()
-    bullets_bottomright = Group()
+# Тестовое создание танка
+tank_topleft = tank.TankTopLeft(screen, 0, 0)
+tank_bottomright = tank.TankBottomRight(screen, 764, 558)
 
-    # Коробки
-    boxes = Group()
+# Установка сложности игры // Оставляйте 1 пока что
+controls.set_difficulty(1)
 
-    # Создание танков
-    tank_topleft = tank.TankTopLeft(screen, 0, 0)
-    tank_bottomright = tank.TankBottomRight(screen, 764, 558)
+# Пули от танка
+bullets_topleft = Group()
+bullets_bottomright = Group()
 
-    # Создание поля. Генерация случайного поля, которое будет использоваться до конца игры.
-    field_of_boxes = field.Field(boxes)
-    field_of_boxes.generate((10, 30), (1, 10))
+# Коробки
+boxes = Group()
 
-    # Создание меню с кнопками "продолжить" и "выйти в главное менню"
-    menu = menu.EscapeMenu(screen)
+# Создание танков
+tank_topleft = tank.TankTopLeft(screen, 0, 0)
+tank_bottomright = tank.TankBottomRight(screen, 764, 558)
 
-    running = True
-    while running:
-        # Количество фпс
-        clock.tick(FPS)
+# Создание поля. Генерация случайного поля, которое будет использоваться до конца игры.
+field_of_boxes = field.Field(boxes)
+field_of_boxes.generate((10, 30), (1, 10))
+
+# Создание меню с кнопками "продолжить" и "выйти в главное менню"
+menu = menu.EscapeMenu(screen)
+
+running = True
+while running:
+    # Количество фпс
+    clock.tick(FPS)
+
+    if main_menu.is_opened == False:
+        # Постоянное отображение заднего фона игры
+        screen.blit(background, (0, 0))
+
+        # Создание поля из коробок каждый раз по новой
+        field_of_boxes.duplicate_screen(screen)
+
+        # Объектам tank_topleft и tank_bottomright передаются набор пуль и коробок
+        tank_topleft.shot(bullets_topleft, boxes, tank_bottomright)
+        tank_bottomright.shot(bullets_bottomright, boxes, tank_topleft)
+
+
+        # Отображение спрайта танка
+        tank_topleft.update()
+        tank_bottomright.update()
+
+        # Объектам tank_topleft и tank_bottomright передается список [(x, y), (x1, y1), ...] с содержанием координат коробок
+        tank_topleft.get_boxes_coordinates([class_instance.coordinates for class_instance in field_of_boxes.boxes])
+        tank_bottomright.get_boxes_coordinates([class_instance.coordinates for class_instance in field_of_boxes.boxes])
+
+        # Нажимаемые клавиши, переменная position для сохранения позиции
+        keys_get_pressed = pg.key.get_pressed()
+
+        # Отрисовка меню, открываемое на кнопку "esc"
+        menu.draw()
+
+        if menu.is_opened == False:
+            """Сюда пишутся все события, которые не должны происходить, когда открывается меню."""
+            # Передаётся класс tank_topleft для понимания направления танка и корректировки относительно его направления пуль
+            bullets_topleft.update(tank_topleft)
+            bullets_bottomright.update(tank_bottomright)
+
+            # Передвижение танка
+            tank_topleft.move(keys_get_pressed, boxes)
+            tank_bottomright.move(keys_get_pressed, boxes)
+
+    elif main_menu.is_opened:
+        main_menu.draw()
+
+    # Обновление экрана
+    pg.display.update()
+
+    for event in pg.event.get():
 
         if main_menu.is_opened == False:
-            # Постоянное отображение заднего фона игры
-            screen.blit(background, (0, 0))
 
-            # Создание поля из коробок каждый раз по новой
-            field_of_boxes.duplicate_screen(screen)
-
-            # Объектам tank_topleft и tank_bottomright передаются набор пуль и коробок
-            tank_topleft.shot(bullets_topleft, boxes, tank_bottomright)
-            tank_bottomright.shot(bullets_bottomright, boxes, tank_topleft)
-
-            # Отображение спрайта танка
-            tank_topleft.update()
-            tank_bottomright.update()
-
-            # Объектам tank_topleft и tank_bottomright передается список [(x, y), (x1, y1), ...] с содержанием координат коробок
-            tank_topleft.get_boxes_coordinates([class_instance.coordinates for class_instance in field_of_boxes.boxes])
-            tank_bottomright.get_boxes_coordinates(
-                [class_instance.coordinates for class_instance in field_of_boxes.boxes])
-
-            # Нажимаемые клавиши, переменная position для сохранения позиции
-            keys_get_pressed = pg.key.get_pressed()
-
-            # Отрисовка меню, открываемое на кнопку "esc"
-            menu.draw()
-
-            if menu.is_opened == False:
-                """Сюда пишутся все события, которые не должны происходить, когда открывается меню."""
-                # Передаётся класс tank_topleft для понимания направления танка и корректировки относительно его направления пуль
-                bullets_topleft.update(tank_topleft)
-                bullets_bottomright.update(tank_bottomright)
-
-                # Передвижение танка
-                tank_topleft.move(keys_get_pressed, boxes)
-                tank_bottomright.move(keys_get_pressed, boxes)
+            menu.open(event, main_menu)
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_SPACE:
+                    tank_topleft.generate_bullet(screen, bullets_topleft, event)
+                if event.key == pg.K_RCTRL:
+                    tank_bottomright.generate_bullet(screen, bullets_bottomright, event)
 
         elif main_menu.is_opened:
-            main_menu.draw()
+            # Если нажата кнопка выхода из игры, то программа должна завершиться.
+           running = main_menu.update(event)
 
-        # Обновление экрана
-        pg.display.update()
-
-        for event in pg.event.get():
-
-            if main_menu.is_opened == False:
-
-                menu.open(event, main_menu)
-                if event.type == pg.KEYDOWN:
-                    if event.key == pg.K_SPACE:
-                        tank_topleft.generate_bullet(screen, bullets_topleft, event)
-                    if event.key == pg.K_RCTRL:
-                        tank_bottomright.generate_bullet(screen, bullets_bottomright, event)
-
-            elif main_menu.is_opened:
-                # Если нажата кнопка выхода из игры, то программа должна завершиться.
-                running = main_menu.update(event)
-
-            if event.type == pg.QUIT:
-                running = False
-                pg.quit()
-    # Закрываем игру
-    pg.quit()
-
+        if event.type == pg.QUIT:
+            running = False
+            pg.quit()
+# Закрываем игру
+pg.quit()
