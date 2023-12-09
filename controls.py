10f82db17660203bcc5efc86d64fd322d9e275c4
Vadim Denisovich <vadimDenis0vich@yandex.com>
2023-12-09 17:08:16 +1000

    Границы спрайтов танков.
    Теперь танки не могут заезжать друг на друга

--- controls.py
+++ controls.py
@@ -17,16 +17,16 @@ def set_difficulty(range):
             tank.Bullet.speed = 5
             field.Box.hp = 150
         case 2:
-            tank.Tank.speed = 1
+            tank.Tank.speed = 2
             tank.Tank.hp = 300
             tank.Tank.shot_delay = 500
             tank.Bullet.damage = 50
-            tank.Bullet.speed = 5
+            tank.Bullet.speed = 8
             field.Box.hp = 200
         case 3:
             tank.Tank.speed = 3
             tank.Tank.hp = 420
             tank.Tank.shot_delay = 500
             tank.Bullet.damage = 40
-            tank.Bullet.speed = 8
+            tank.Bullet.speed = 10
             field.Box.hp = 200
